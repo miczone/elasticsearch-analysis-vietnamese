c25a7cf3b9d039ccde104c654eb72aa081605d77
BioTuring <bioturing@BioTurings-MacBook-Pro.local>
2022-11-22 22:51:48 +0700

    Fix search 8.5.1

--- src/main/java/org/elasticsearch/index/analysis/VietnameseAnalyzerProvider.java
+++ src/main/java/org/elasticsearch/index/analysis/VietnameseAnalyzerProvider.java
@@ -20,7 +20,6 @@ import org.elasticsearch.analysis.VietnameseConfig;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
-import org.elasticsearch.index.analysis.AbstractIndexAnalyzerProvider;
 
 /**
  * Provider for {@link VietnameseAnalyzer}
